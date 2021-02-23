9f60a77cf6fd62230f1e336a6e4e6ccf466adf1e
Thomas <thomaspegg.linck@gmail.com>
2021-02-22 21:14:27 -0300

    create eclipse project

---
+++ Imprimir-ASCII/src/Imprimir-ASCII.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Imprimir-ASCII.cpp
+// Author      : Thomas
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	string helloWorld = "Hello World";
+
+	cout << helloWorld << endl; // prints !!!Hello World!!!
+
+	return 0;
+}
