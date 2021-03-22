8814c9cf8e215ae304e02407449f2b1b10a1342c
Thomas <thomaspegg.linck@gmail.com>
2021-03-22 20:25:19 -0300

    corrections

--- Lista-Encadeada/src/Lista-Encadeada.cpp
+++ Lista-Encadeada/src/Lista-Encadeada.cpp
@@ -9,7 +9,36 @@
 #include <iostream>
 using namespace std;
 
+struct TouristAttractionModel{
+	string description;
+	float latitude;
+	float longitude;
+	struct TouristAttractionModel * nextTouristAttraction;
+};
+
+typedef struct TouristAttractionModel TouristAttraction;
+typedef TouristAttraction * TouristAttractionPointer;
+
+class TouristAttractionsListManager{
+
+	public:
+		TouristAttractionPointer createNewList(){
+			return NULL;
+		}
+
+		void setHeaderToExistingList(TouristAttractionPointer existingListHeader){
+			listHeader = existingListHeader;
+		}
+
+	private:
+		TouristAttractionPointer listHeader;
+
+};
+
+
 int main() {
+
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
 	return 0;
 }
