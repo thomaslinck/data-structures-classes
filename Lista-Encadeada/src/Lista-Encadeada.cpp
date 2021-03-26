08ee8d6840196fa64d75bd8cd06b5718ed141f1f
Thomas <thomaspegg.linck@gmail.com>
2021-03-26 18:55:36 -0300

    general settings

--- Lista-Encadeada/src/Lista-Encadeada.cpp
+++ Lista-Encadeada/src/Lista-Encadeada.cpp
@@ -30,15 +30,70 @@ class TouristAttractionsListManager{
 			listHeader = existingListHeader;
 		}
 
+		void addNewTouristAttraction(TouristAttraction touristAttraction){
+
+		}
+
 	private:
 		TouristAttractionPointer listHeader;
 
 };
 
+class Application{
+	public:
+
+		Application(){
+			touristAttractionsListManager.createNewList();
+		}
+
+		void execute(){
+			while (defineListOperation() == '1'){
+				touristAttractionsListManager.
+					addNewTouristAttraction(
+						readTouristAttraction());
+			}
+		}
+
+	private:
+
+		TouristAttraction readTouristAttraction(){
+					TouristAttraction touristAttraction;
+
+					cout << "Digite a descrição do ponto turístico" << endl;
+					cin >> touristAttraction.description;
+
+					cout << "Digite a latitude do ponto turístico" << endl;
+					cin >> touristAttraction.latitude;
+
+					cout << "Digite a longitude do ponto turístico" << endl;
+					cin >> touristAttraction.longitude;
+
+					return touristAttraction;
+				}
+
+		char defineListOperation(){
+			char operation;
+
+			cout << "As operações disponíveis para a lista são: "<< endl;
+			cout << "1 - Adicionar" << endl;
+			cout << "2 - Sair" << endl;
+
+			cin >> operation;
+
+			return operation;
+		}
+
+		TouristAttractionsListManager touristAttractionsListManager;
+};
+
 
 int main() {
 
+	Application application;
+
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	application.execute();
 
 	return 0;
 }
