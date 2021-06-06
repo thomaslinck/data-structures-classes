ca81bbf9a0e40c37a8ff46cc8c0a03ce708f4b0f
Thomas <thomaspegg.linck@gmail.com>
2021-06-06 15:35:50 -0300

    package reorganization

--- sorting/python_project/dependencies/sorting_functions/selection_sort.py
+++ sorting/python_project/dependencies/sorting_functions/selection_sort.py
@@ -1,11 +1,8 @@
-from commons import swap_elements
-
-
 def selection_sort(sorting_list):
     for i in range(len(sorting_list)):
         maior = 0
         for j in range(len(sorting_list)-i):
             if sorting_list[j] > sorting_list[maior]:
                 maior = j
-        swap_elements(sorting_list, maior, j)
+        sorting_list[maior], sorting_list[j] = sorting_list[j], sorting_list[maior]
     return sorting_list
